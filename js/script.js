ba6033424d65c2da4e5fcafe7f06966940ee612e
Andrew Meyer <andrew@rewdy.com>
2013-12-25 10:36:16 -0600

    Set up parallax effect on header image and refined the JS to not overload the scroll event handler.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
-// Script.jss
+/*
+
+Readium Theme
+-------------
+
+script.js
+
+Readium theme javascript. Uses jQuery (obviously).
+
+*/
 
 $(function(){
-	// if ($('body').hasClass('read')) {
-	// 	var scrollDestination = $('#content-body').offset().top;
-	// 	$('html, body').animate({scrollTop:scrollDestination}, 2000);
-	// }
-
-	// drawer toggle
+	// Drawer toggle
 	$('#navigation-toggle-link').click(function(){
 		$('#control').toggleClass('open');
 		return false;
-	})
+	});
 
-	// sharing stuff
-	$('a.share-link').click(function(){
-		$(this).closest('article').find('.m-share').addClass('m-show');
-		return false;
+	// Set pageTop variable for other functions to use
+	window.scrolled = false;
+	window.pageTop = $('html, body').scrollTop();
+	$(window).scroll(function(){
+		window.scrolled = true;
+		window.pageTop = $('html, body').scrollTop();
 	});
-	$('a.perma-link').click(function(){
-		$(this).closest('article').find('.m-permalink').addClass('m-show');
-		$(this).closest('article').find('.m-permalink input.permalink').click(function(){
-			$(this).select();
-			return false;
-		});
-		return false;
-	});
-	$('a.m-close-link').click(function(){
-		$(this).closest('.m-show').removeClass('m-show');
-		return false;
-	});
-	$('.sharing-list a').not('.email').click(function(){
-		href = $(this).attr('href');
-		openWindow(href, 'Share');
-		return false;
-	});
+
+	// Parallax effect on header image
+	var header_height = $('#site-header').not('.no-image').height();
+	var backgroundOffset = 0;
+	if (header_height) {
+		setInterval(function(){
+			if (window.pageTop < header_height) {
+				backgroundOffset = (window.pageTop/2) * -1;
+				$('#deco').css('background-position', 'center ' + backgroundOffset + 'px');
+			}
+		}, 5);
+	}
+
+	enableSharing();
 });
 
 $(window).load(function(){
@@ -45,23 +48,46 @@ $(window).load(function(){
 		var articleTop = article.offset().top;
 		var articleBottom = articleTop + article.outerHeight();
 		var calculationPadding = 400; // this is extra space to add when calculating the percentage because people don't read at the top of their screens.
-		$(window).scroll(function(){
-			var top = $('html, body').scrollTop();
-			if (top >= articleTop && top <= articleBottom) {
-				if (!enabled) {
-					enabled = true
-				}
-				var percentageFinished = Math.round((top - articleTop) / (articleBottom - articleTop - calculationPadding) * 100);
-				line.width(percentageFinished + '%');
-			} else {
-				if (enabled) {
+
+		// checked if the page has scrolled and sets the readline accordingly
+		setInterval(function(){
+			if (window.scrolled) {
+				var top = window.pageTop;
+				if (top >= articleTop && top <= articleBottom) {
+					var percentageFinished = Math.round((top - articleTop) / (articleBottom - articleTop - calculationPadding) * 100);
+					line.width(percentageFinished + '%');
+				} else {
 					line.width(0);
-					enabled = false;
 				}
 			}
-		})
+		}, 50);
 	});
 });
+
+function enableSharing() {
+	// Sharing: modal behavior and sharing links
+	$('a.share-link').click(function(){
+		$(this).closest('article').find('.m-share').addClass('m-show');
+		return false;
+	});
+	$('a.perma-link').click(function(){
+		$(this).closest('article').find('.m-permalink').addClass('m-show');
+		return false;
+	});
+	$('.m-permalink input.permalink').click(function(){
+		$(this).select();
+		return false;
+	});
+	$('a.m-close-link').click(function(){
+		$(this).closest('.m-show').removeClass('m-show');
+		return false;
+	});
+	$('.sharing-list a').not('.email').click(function(){
+		href = $(this).attr('href');
+		openWindow(href, 'Share');
+		return false;
+	});
+}
 
 // function to open urls in a new window (used for sharing)
 function openWindow(url, title) {
