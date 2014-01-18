dd50373181693264aa4529236d142a4ecfc4a7ae
Andrew Meyer <andrew@rewdy.com>
2014-01-18 09:49:17 -0600

    Fixed some bugs with the drawer.

--- drawer.php
+++ drawer.php
@@ -12,18 +12,19 @@ Drawer template file
 
 			<div id="drawer">
 				<a id="navigation-toggle-link" href="#drawer"><i class="fa fa-bars"></i> <span class="text">Menu</span></a>
-				<nav id="access" role="navigation">
-					<h2 class="assistive-text">Main menu</h2>
-					<?php
-						$menu_args = array(
-							'theme_location' => 'primary',
-							'container' => false,
-							'menu_class' => 'menu',
-						);
-					?>
-					<?php wp_nav_menu($menu_args); ?>
-				</nav>
+				<div id="drawer-content">
+					<nav id="access" role="navigation">
+						<h2 class="assistive-text">Main menu</h2>
+						<?php
+							$menu_args = array(
+								'theme_location' => 'primary',
+								'container' => false,
+								'menu_class' => 'menu',
+							);
+						?>
+						<?php wp_nav_menu($menu_args); ?>
+					</nav>
 
-				<?php get_sidebar('drawer'); ?>
-
+					<?php get_sidebar('drawer'); ?>
+				</div>
 			</div> <!-- close #drawer -->
